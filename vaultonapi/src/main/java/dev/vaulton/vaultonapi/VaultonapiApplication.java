0eb8b540fb6a65936d64bd110bb4f8ad4e9e541f
Patryk Sowiński <79578683+Treszyk@users.noreply.github.com>
2026-04-25 00:38:01 +0200

    feat: containerize java backend and update compose orchestration (#2)
    * chore: re-enable database autoconfiguration
    
    * chore: integrate java api into docker-compose orchestration
    
    * fix: use h2 in-memory database for tests to decouple from postgres

--- vaultonapi/src/main/java/dev/vaulton/vaultonapi/VaultonapiApplication.java
+++ vaultonapi/src/main/java/dev/vaulton/vaultonapi/VaultonapiApplication.java
@@ -2,9 +2,8 @@ package dev.vaulton.vaultonapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class VaultonapiApplication {
 
 	public static void main(String[] args) {
