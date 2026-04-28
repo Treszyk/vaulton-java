24ded7aef3a2eac0784fb9c4c7a157b7dbe8b056
Treszyk <79578683+Treszyk@users.noreply.github.com>
2026-04-28 15:11:18 +0200

    refactor: introduce helper methods for cleaner mock buffers in tests

--- vaultonapi/src/test/java/dev/vaulton/vaultonapi/domain/crypto/EncryptedValueTest.java
+++ vaultonapi/src/test/java/dev/vaulton/vaultonapi/domain/crypto/EncryptedValueTest.java
@@ -7,39 +7,42 @@ import java.nio.charset.StandardCharsets;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EncryptedValueTest {
+    private SecureBuffer getMockSecureBuffer(int length) {
+        return new SecureBuffer(new String(new char[length]).replace('\0', 'a').getBytes(StandardCharsets.UTF_8)); // 4
+    }
+
+    private SecureBuffer mockNonce(boolean valid) {
+        return getMockSecureBuffer(CryptoConstants.GCM_NONCE_LEN + (valid ? 0 : 1));
+    }
+
+    private SecureBuffer mockCipherText(boolean valid) {
+        return getMockSecureBuffer(CryptoConstants.MAX_ENTRY_CIPHERTEXT_BYTES + (valid ? 0 : 1));
+    }
+
+    private SecureBuffer mockTag(boolean valid) {
+        return getMockSecureBuffer(CryptoConstants.GCM_TAG_LEN + (valid ? 0 : 1));
+    }
+
     @Test
     void shouldThrowWhenInvalidCryptoSize() {
         assertThrows(IllegalArgumentException.class, () -> {
-                new EncryptedValue(
-                        new SecureBuffer("ąąąąąąą".getBytes(StandardCharsets.UTF_8)), // 14 bytes instead of 12
-                        new SecureBuffer("cipherText".getBytes(StandardCharsets.UTF_8)),
-                        new SecureBuffer("ąąąąąąąą".getBytes(StandardCharsets.UTF_8))
-                );
+            // 13 bytes Nonce instead of 12
+            new EncryptedValue(mockNonce(false), mockCipherText(true), mockTag(true));
         });
         assertThrows(IllegalArgumentException.class, () -> {
-            new EncryptedValue(
-                    new SecureBuffer("ąąąąąą".getBytes(StandardCharsets.UTF_8)),
-                    new SecureBuffer("cipherText".getBytes(StandardCharsets.UTF_8)),
-                    new SecureBuffer("ąąąąąąąąą".getBytes(StandardCharsets.UTF_8)) // 18 bytes instead of 16
-            );
+            // 4097 bytes CipherText instead of 4096
+            new EncryptedValue(mockNonce(true), mockCipherText(false), mockTag(true));
         });
         assertThrows(IllegalArgumentException.class, () -> {
-            new EncryptedValue(
-                    new SecureBuffer("ąąąąąą".getBytes(StandardCharsets.UTF_8)),
-                    new SecureBuffer(new String(new char[CryptoConstants.MAX_ENTRY_CIPHERTEXT_BYTES+1]).replace('\0', 'ą').getBytes(StandardCharsets.UTF_8)), // 4098 bytes instead of 4096
-                    new SecureBuffer("ąąąąąąąą".getBytes(StandardCharsets.UTF_8))
-            );
+            // 17 bytes Tag instead of 16
+            new EncryptedValue(mockNonce(true), mockCipherText(true), mockTag(false));
         });
     }
 
     @Test
     void shouldCreateCorrectlyWhenValidCryptoSize() {
         assertDoesNotThrow(() -> {
-            new EncryptedValue(
-                    new SecureBuffer("ąąąąąą".getBytes(StandardCharsets.UTF_8)), // 12 bytes
-                    new SecureBuffer(new String(new char[CryptoConstants.MK_LEN]).replace('\0', 'ą').getBytes(StandardCharsets.UTF_8)), // 4096 bytes
-                    new SecureBuffer("ąąąąąąąą".getBytes(StandardCharsets.UTF_8)) // 16 bytes
-            );
+            new EncryptedValue(mockNonce(true), mockCipherText(true), mockTag(true));
         });
     }
 }
