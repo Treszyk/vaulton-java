99ad58a2f78d6f585724f635220a5f3ed4a382f4
Treszyk <79578683+Treszyk@users.noreply.github.com>
2026-04-30 14:49:30 +0200

    feat: implement persistence repository adapters

--- vaultonapi/src/main/java/dev/vaulton/vaultonapi/infrastructure/persistence/repository/UserJpaRepository.java
+++ vaultonapi/src/main/java/dev/vaulton/vaultonapi/infrastructure/persistence/repository/UserJpaRepository.java
@@ -4,4 +4,6 @@ import dev.vaulton.vaultonapi.infrastructure.persistence.entity.UserEntity;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserJpaRepository extends JpaRepository<UserEntity, UUID> {}
+public interface UserJpaRepository extends JpaRepository<UserEntity, UUID> {
+  long removeById(UUID id);
+}
